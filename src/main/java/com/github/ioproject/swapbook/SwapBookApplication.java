a467cb2c823f73e06d07cfbbc24be808334587a6
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2019-10-31 00:59:20 +0100

    Create init repo from spring initializer

---
+++ src/main/java/com/github/ioproject/swapbook/SwapBookApplication.java
@@ -0,0 +1,13 @@
+package com.github.ioproject.swapbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ioproject/swapbook/SwapBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ioproject.swapbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
