8a0bb72866235806076a445e58fe362bcc231483
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2019-11-01 01:51:23 +0100

    Small cleanup in main application class

--- service/src/main/java/com/github/swapbook/SwapBookApplication.java
+++ service/src/main/java/com/github/swapbook/SwapBookApplication.java
@@ -4,23 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
 
 @SpringBootApplication
 public class SwapBookApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(SwapBookApplication.class);
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(SwapBookApplication.class, args);
+
+		log.info("SwapBookApplication has just started");
 	}
 
 
