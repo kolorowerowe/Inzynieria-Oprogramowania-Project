d773ac513fa84a5b6f89fc7c56fa2c687cdf76f2
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2019-12-02 17:52:31 +0100

    Divide unlogged home screen to divs and add form to login

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "./Home.css"
+import {Accordion, Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 
 
 class Home extends Component {
@@ -17,9 +18,64 @@ class Home extends Component {
 
     render() {
         return (
-            <div className="Homes">
-                <h2>Home</h2>
-                <p>elo</p>
+            <div className="Home">
+                <Container id="home_unlogged">
+                    <Row>
+                        <Col md={6}  className="login-register login">
+
+                            <Accordion defaultActiveKey="0">
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="1">
+                                        <h2>
+                                            Zaloguj
+                                        </h2>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="1">
+                                        <Card.Body>
+                                            <Form>
+                                                <Form.Group as={Row} controlId="formHorizontalEmail">
+                                                    <Form.Label column sm={3}>
+                                                        Email
+                                                    </Form.Label>
+                                                    <Col sm={9}>
+                                                        <Form.Control type="email" placeholder="Email" />
+                                                    </Col>
+                                                </Form.Group>
+                                                <Form.Group as={Row} controlId="formHorizontalPassword">
+                                                    <Form.Label column sm={3}>
+                                                        Password
+                                                    </Form.Label>
+                                                    <Col sm={9}>
+                                                        <Form.Control type="password" placeholder="Password" />
+                                                    </Col>
+                                                </Form.Group>
+                                                <Form.Group as={Row}>
+                                                    <Col sm={{ span: 10, offset: 2 }}>
+                                                        <Button type="submit">Sign in</Button>
+                                                    </Col>
+                                                </Form.Group>
+                                            </Form>
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                            </Accordion>
+
+
+                        </Col>
+                        <Col md={6}>
+                            <h2 className="login-register register">
+                                Zarejestruj się
+                            </h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h2 className="login-register-info">
+                                Info
+                            </h2>
+                        </Col>
+                    </Row>
+                </Container>
 
             </div>
         );
