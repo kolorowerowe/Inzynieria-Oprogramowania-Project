b7550c06db09013b9107d939da2bf507e5e8ac52
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2019-12-13 12:21:47 +0100

    Add creating a database SQL script

--- my_commends.sql
+++ my_commends.sql
@@ -1,11 +1,63 @@
-CREATE TABLE Users (
-    id int,
-    name varchar(255),
-    email varchar(255),
-    password varchar(255),
-    address varchar(255),
-    specimenList varchar(255)
+DROP SCHEMA IF EXISTS swapbook CASCADE;
+CREATE SCHEMA IF NOT EXISTS swapbook;
+
+CREATE TABLE swapbook.users (
+    id INT PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    email VARCHAR (255) UNIQUE NOT NULL,
+    password VARCHAR (255) NOT NULL,
+    address VARCHAR (255)
 );
+
+CREATE TABLE swapbook.opinie (
+    id INT PRIMARY KEY,
+    id_wystawiajacy INT REFERENCES swapbook.users,
+    id_oceniany INT REFERENCES swapbook.users,
+    tresc VARCHAR (500),
+    ocena INT NOT NULL CHECK ((idkompozycji >= 1) AND ( idkompozycji <= 5 )),
+    data DATE
+);
+
+CREATE TABLE swapbook.ksiazki (
+    id INT PRIMARY KEY,
+    tytul VARCHAR (255) UNIQUE NOT NULL,
+    autor VARCHAR (255)  NOT NULL,
+    zdjecie_url VARCHAR (255) NOT NULL --url?
+);
+
+CREATE TABLE swapbook.recenzje(
+    id INT PRIMARY KEY,
+    ksiazkaID INT REFERENCES swapbook.ksiazki,
+    userID INT REFERENCES swapbook.users,
+    tresc VARCHAR (500),
+    ocena INT NOT NULL CHECK ((ocena >= 1) AND ( ocena <= 5 )),
+    data DATE
+);
+
+CREATE TABLE swapbook.egzemplarze(
+    id INT PRIMARY KEY,
+    ksiazkaID INT REFERENCES swapbook.ksiazki,
+    tytul VARCHAR (255) NOT NULL,
+    stan VARCHAR(64),
+    lStron INT ,
+    autor VARCHAR (255) NOT NULL,
+    dataWydania DATE,
+    nrWydania VARCHAR (64),
+    ISBN VARCHAR (64),
+    czas_wypozyczenia DATE,
+    zdjecie_url VARCHAR (255) NOT NULL -- url?
+);
+
+CREATE TABLE swapbook.wypozyczenia(
+    id INT PRIMARY KEY,
+    egzemplarzID INT REFERENCES swapbook.egzemplarze,
+    wlascicielID INT REFERENCES swapbook.users,
+    czytelnikID INT REFERENCES swapbook.users,
+    stan VARCHAR(64),                           -- tutaj z jakiejś listy rozwijanej, 1-5 czy jak?
+    data_wyp DATE NOT NULL,
+    data_odbioru DATE,
+    dl_wyp INT                                   -- w dniach?
+)
 
 INSERT INTO Users
 VALUES (12, 'Dominik', 'kolodziejd@student.agh.edu.pl', 'xxx','Brzezówka 180','12,34,53');
