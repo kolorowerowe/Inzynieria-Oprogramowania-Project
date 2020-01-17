8e1c22659af38d903d8239964f9a40436511e81a
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2020-01-17 22:07:41 +0100

    Forgot to add this file

---
+++ src/main/java/com/github/swapbook/repositories/loans/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.github.swapbook.repositories.loans;
+
+import com.github.swapbook.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Integer> {
+}
